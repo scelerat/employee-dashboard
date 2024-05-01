52d8631616e6dc1612398633282d4e1a1a7eb974
David Kurtz <david@scelerat.com>
2024-05-01 00:02:22 -0700

    app.delete uses kysely

--- backend/index.ts
+++ backend/index.ts
@@ -3,7 +3,8 @@ import { Client } from 'pg';
 import {
   findEmployee,
   updateEmployee,
-  createEmployee
+  createEmployee,
+  deleteEmployee
 } from './EmployeeRepository'
 import { employeeRequestParamsToDb } from './utils';
 import dotenv from 'dotenv';
@@ -110,11 +111,8 @@ app.put('/employees/:id', async (req: Request, res: Response) => {
 app.delete('/employees/:id', async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
-    const result = await client.query(
-      'DELETE FROM employees WHERE id = $1 RETURNING *',
-      [id]
-    );
-    if (result.rows.length === 0) {
+    const deletedEmployee = await deleteEmployee(parseInt(id, 10));
+    if (!deletedEmployee) {
       res.status(404).json({ error: 'Employee not found' });
     } else {
       res.json({ message: 'Employee deleted successfully' });
